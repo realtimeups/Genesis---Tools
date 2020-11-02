14da281ed6e3ac301dad5a68f4128ca308f7fbc9
Matin-Delta <68481709+Matin-Delta@users.noreply.github.com>
2020-11-02 20:00:09 +0330

    Update index.js

--- index.js
+++ index.js
@@ -347,40 +347,6 @@ client.on('messageDelete', async message => {
 
 
 });
-cron.schedule('* 10 * * * *', function() {
-    const meme = client.channels.cache.get(process.env.meme);
-    let reddit = [
-        "meme",
-        "animemes",
-        "MemesOfAnime",
-        "animememes",
-        "AnimeFunny",
-        "dankmemes",
-        "dankmeme",
-        "wholesomememes",
-        "MemeEconomy",
-        "techsupportanimals",
-        "meirl",
-        "me_irl",
-        "2meirl4meirl",
-        "AdviceAnimals"
-    ]
-
-    let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-
-    meme.startTyping();
-
-    randomPuppy(subreddit).then(async url => {
-        await meme.send({
-            files: [{
-                attachment: url,
-                name: 'meme.png'
-            }]
-        }).then(() => meme.stopTyping());
-    }).catch(err => console.error(err));
-
-    console.log("Meme Sended")
-});
 client.login(process.env.token);
 
 /*
