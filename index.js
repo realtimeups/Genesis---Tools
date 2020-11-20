735e0ca8423f1c729f75c58514e043536e409074
Matin-Delta <68481709+Matin-Delta@users.noreply.github.com>
2020-11-21 00:16:21 +0330

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,15 @@ client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
-    if(command === 'among'){
+     if(command === 'among'){
         if(!args.length){
             return message.channel.send("```md\n# Err \n#Usage: !!among <room code>\n```")
         }
         if(args.length){
-            (await message.guild.channels.create(`Code: ${args[0]}`)).setParent("779437695590989824");
-
+            await message.guild.channels.create(`💠➢Code: ${args[0]}`, {
+                type: 'voice',
+                parent: "779437695590989824"
+            });
             const channel = message.guild.channels.get("779439251241959475")
             channel.send(`@everyone \nGame Ba Code ${args[0]} Shoro Shod Dus Dashtid Berid Play Bedid<a:A9:683287687427391497>`)
         }
